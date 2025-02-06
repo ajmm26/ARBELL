a211a7d727df95d93337aad8b450a40bd9e1c7cf
Alejandro Maza <ajmm9257@gmail.com>
2025-02-06 19:57:41 -0300

    cambio de nombre public por docs

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const app = express();
 const PORT = 3000;
 
 // Servir archivos estáticos desde la carpeta 'public'
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'docs')));
 
 // Servir el archivo index.html en la ruta raíz "/"
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'docs', 'index.html'));
 });
 
 // Iniciar el servidor
